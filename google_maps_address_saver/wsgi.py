8610725b1c7d7785eab141ca22d95b5cacd48abf
simonmelouah <simonmelouah@gmail.com>
2018-08-09 20:50:08 +0100

    Project dirs

---
+++ google_maps_address_saver/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for google_maps_address_saver project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_maps_address_saver.settings')
+
+application = get_wsgi_application()
