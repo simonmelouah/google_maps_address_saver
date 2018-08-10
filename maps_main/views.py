461a9ad45ba0059e392d21ae31a7816b47efd920
simonmelouah <simonmelouah@gmail.com>
2018-08-10 16:18:23 +0100

    Preventing duplicates

--- maps_main/views.py
+++ maps_main/views.py
@@ -12,6 +12,7 @@ from google_install.views import check_access_token
 def maps_main_home(request):
     """Main home page that renders the map."""
     if request.method == "GET":
+        address_object = Address.objects.filter(fusion_table__google_id=request.session.get("fusion_table_id")).all()
         sql = f"SELECT * FROM {request.session.get('fusion_table_id')}"
         fusion_table_values = requests.get(
             url=f"https://www.googleapis.com/fusiontables/v2/query?sql={sql}",
@@ -20,29 +21,32 @@ def maps_main_home(request):
                 "Content-Type": "application/json"}).json()
         return render(request, 'maps_main.html',
                       {'fusion_table_rows': fusion_table_values.get("rows"),
+                       'address_data': address_object,
                        'map_centre_lat': fusion_table_values.get("rows")[0][1] if fusion_table_values.get("rows") else None,
                        'map_centre_lng': fusion_table_values.get("rows")[0][2] if fusion_table_values.get("rows") else None})
     address_json = json.loads(request.body)
     lat = address_json.get('geometry')['location']['lat']
     lng = address_json.get('geometry')['location']['lng']
     formatted_address = address_json.get('formatted_address')
-    insert_sql = f"INSERT INTO {request.session.get('fusion_table_id')} (address, lat, lng, created_at) VALUES ('{formatted_address}', {lat}, {lng}, '{datetime.datetime.now()}');"
-    store_address = requests.post(
-        url=f"https://www.googleapis.com/fusiontables/v2/query?sql={insert_sql}",
-        headers={
-            "Authorization": f"Bearer {request.session.get('access_token')}",
-            "Content-Type": "application/json"})
-    if store_address.status_code != 200:
-        print(request.session.get('access_token'), store_address.text)
-        return HttpResponse(status=store_address.status_code)
-    fusion_table_object = FusionTable.objects.filter(
-        google_id=request.session.get('fusion_table_id')).first()
-    if not fusion_table_object:
-        return HttpResponse(status=500)
-    Address(
-        fusion_table=fusion_table_object,
-        latitude=lat,
-        longitude=lng,
-        full_address=formatted_address
-    ).save()
+    address_object = Address.objects.filter(full_address=formatted_address).first()
+    if not address_object:
+        insert_sql = f"INSERT INTO {request.session.get('fusion_table_id')} (address, lat, lng, created_at) VALUES ('{formatted_address}', {lat}, {lng}, '{datetime.datetime.now()}');"
+        store_address = requests.post(
+            url=f"https://www.googleapis.com/fusiontables/v2/query?sql={insert_sql}",
+            headers={
+                "Authorization": f"Bearer {request.session.get('access_token')}",
+                "Content-Type": "application/json"})
+        if store_address.status_code != 200:
+            print(request.session.get('access_token'), store_address.text)
+            return HttpResponse(status=store_address.status_code)
+        fusion_table_object = FusionTable.objects.filter(
+            google_id=request.session.get('fusion_table_id')).first()
+        if not fusion_table_object:
+            return HttpResponse(status=500)
+        Address(
+            fusion_table=fusion_table_object,
+            latitude=lat,
+            longitude=lng,
+            full_address=formatted_address
+        ).save()
     return HttpResponse(status=200)
