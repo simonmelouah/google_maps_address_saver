134cc2c5ed0039e721f40c026d38a912fd16805b
simonmelouah <simonmelouah@gmail.com>
2018-08-10 11:24:23 +0100

    Inserting data intop

--- maps_main/views.py
+++ maps_main/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.http import require_http_methods
 import datetime
 import json
 import requests
+from .models import FusionTable, Address
+
 
 @require_http_methods(["GET", "POST"])
 def maps_main_home(request):
@@ -14,10 +16,21 @@ def maps_main_home(request):
     lng = address_json.get('geometry')['location']['lng']
     formatted_address = address_json.get('formatted_address')
     insert_sql = f"INSERT INTO {request.session.get('fusion_table_id')} (address, coordinates, created_at) VALUES ('{formatted_address}', '{lat} {lng}', '{datetime.datetime.now()}');"
-    print(insert_sql)
-    all_data = requests.post(url=f"https://www.googleapis.com/fusiontables/v2/query?sql={insert_sql}",
-                             headers={
-                                "Authorization": f"Bearer {request.session.get('access_token')}",
-                                "Content-Type": "application/json"})
-    print(all_data.text)
+    store_address = requests.post(
+        url=f"https://www.googleapis.com/fusiontables/v2/query?sql={insert_sql}",
+        headers={
+            "Authorization": f"Bearer {request.session.get('access_token')}",
+            "Content-Type": "application/json"})
+    if store_address.status_code != 200:
+        return HttpResponse(status=store_address.status_code)
+    fusion_table_object = FusionTable.objects.filter(google_id=request.session.get('fusion_table_id')).first()
+    if not fusion_table_object:
+        return HttpResponse(status=500)
+    Address(
+        fusion_table=fusion_table_object,
+        latitude=lat,
+        longitude=lng,
+        full_address=formatted_address
+    ).save()
+    print("Saved")
     return HttpResponse(status=200)
